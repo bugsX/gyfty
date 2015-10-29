2b7aced7413504e029ee35e18128dc4fb2ff9245
akhilch <cakhilpriyesh@gmail.com>
2015-10-29 12:38:39 -0700

    Changed Gyfty Attributes to accepts lists strings and objects

--- app/src/main/java/com/gyfty/attributes/GyftyAttributes.java
+++ app/src/main/java/com/gyfty/attributes/GyftyAttributes.java
@@ -1,21 +1,14 @@
 package com.gyfty.attributes;
 
-import android.support.annotation.NonNull;
-
+import com.google.common.collect.Maps;
 import com.parse.FindCallback;
-import com.parse.Parse;
 import com.parse.ParseClassName;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import javax.security.auth.callback.Callback;
 
 /**
  * Created by Mac on 10/4/15.
@@ -24,43 +17,16 @@ import javax.security.auth.callback.Callback;
 @ParseClassName("GyftyAttributes")
 public class GyftyAttributes extends ParseObject{
 
-    public String getAttributeValue() {
-        return getString(GyftyAttributesParams.attributeValue.toString());
-    }
-
-    public void setAttributeValue(String value) {
-        put(GyftyAttributesParams.attributeValue.toString(),value);
-    }
-
-    public String getAttributeName() {
-        return getString(GyftyAttributesParams.attributeName.toString());
-    }
-
-    public void setAttributeName(String value) {
-        put(GyftyAttributesParams.attributeName.toString(),value);
-    }
-
-
-    enum GyftyAttributesParams {
-
-        attributeValue,
-        attributeName // Delivery Charge, Pin-code Checking
-
-    }
-
-    public static Map<String, String> attributeMap = new HashMap<String,String>();
+    public static Map<String, Object> attributeMap = Maps.newHashMap();
 
     public static void loadAttributeMap() {
-        ParseQuery<GyftyAttributes> attributesQuery = new ParseQuery("GyftyAttributes");
+        ParseQuery<GyftyAttributes> attributesQuery = new ParseQuery<>("GyftyAttributes");
         attributesQuery.findInBackground(new FindCallback<GyftyAttributes>() {
             @Override
             public void done(List<GyftyAttributes> list, ParseException e) {
-
                 if (e == null) {
-
-                    for(GyftyAttributes attribute:list){
-
-                        attributeMap.put(attribute.getAttributeName(),attribute.getAttributeValue());
+                    for (GyftyAttributes attribute : list) {
+                        attributeMap.put(attribute.getAttributeName(), attribute.getAttributeValue());
 
                     }
 
@@ -70,6 +36,28 @@ public class GyftyAttributes extends ParseObject{
         });
 
 
+    }
+
+    public Object getAttributeValue() {
+        return get(GyftyAttributesParams.attributeValue.toString());
+    }
+
+    public void setAttributeValue(Object value) {
+        put(GyftyAttributesParams.attributeValue.toString(),value);
+    }
+
+    public String getAttributeName() {
+        return getString(GyftyAttributesParams.attributeName.toString());
+    }
+
+    public void setAttributeName(String value) {
+        put(GyftyAttributesParams.attributeName.toString(), value);
+    }
+
+    enum GyftyAttributesParams {
+
+        attributeValue,
+        attributeName // Delivery Charge, Pin-code Checking
 
     }
 
